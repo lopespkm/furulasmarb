68ca6413c1d8d34f5cba8074a919680f0d78c754
lopespkm <59057440+lopespkm@users.noreply.github.com>
2025-07-29 22:29:58 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Desativa otimização de imagens
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.raspadinhafortune.shop',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ultrapanel.shop',
+        pathname: '/**', // permite qualquer imagem do domínio
+      },
+      {
+        protocol: 'https',
+        hostname: 'sucessogrupo.shop',
+        pathname: '/imagemsras/**', // ajuste conforme necessário
+      },
+    ],
   },
 };
 
